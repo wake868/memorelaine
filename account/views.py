b3e2a08693ab8241128b8f783f9764349095ecc1
wake868 <wake868@gmail.com>
2020-04-28 18:03:06 +0000

    added bootstrap css and shared base template

--- account/views.py
+++ account/views.py
@@ -1,5 +1,9 @@
 # views.py -> account
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
 
 
 def home(request):
@@ -7,7 +11,21 @@ def home(request):
 
 
 def signup_account(request):
-    return render(request, 'account/signup_account.html')
+    if request.method == 'GET':
+        return render(request, 'account/signup_account.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('memorywall:home')
+            except IntegrityError:
+                return render(request, 'account/signup_account.html', {'form': UserCreationForm(), 'error': 'Username already in use. Please choose a different username.'})
+
+        else:
+            print("hello")
+            return render(request, 'account/signup_account.html', {'form': UserCreationForm(), 'error': 'Passwords did not match.'})
 
 
 def login_account(request):
